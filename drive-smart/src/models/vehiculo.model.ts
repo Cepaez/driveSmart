fd9569eb1e92b2cb2bf41b4fec725117bc031bb9
jhoncarlos79 <jhoncarlos@gmail.com>
2022-10-29 20:15:28 -0500

    Creacion Cliente, Solicitud, Vehiculo

---
+++ drive-smart/src/models/vehiculo.model.ts
@@ -0,0 +1,57 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Vehiculo extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  direccion: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  ciudad: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  valor: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  tipooferta: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  foto: string;
+
+  @property({
+    type: 'string',
+  })
+  video?: string;
+
+
+  constructor(data?: Partial<Vehiculo>) {
+    super(data);
+  }
+}
+
+export interface VehiculoRelations {
+  // describe navigational properties here
+}
+
+export type VehiculoWithRelations = Vehiculo & VehiculoRelations;
