c5c24242114f4223866207f828807841b93812ba
jhoncarlos79 <jhoncarlos@gmail.com>
2022-10-30 20:52:56 -0500

    Eliminacion Entidad Invitacion

--- drive-smart/src/repositories/index.ts
+++ drive-smart/src/repositories/index.ts
@@ -1,7 +1,6 @@
 export * from './asesor.repository';
 export * from './tipo-vehiculo.repository';
 export * from './administrador.repository';
-export * from './invitacion.repository';
 export * from './cliente.repository';
 export * from './solicitud.repository';
 export * from './vehiculo.repository';
